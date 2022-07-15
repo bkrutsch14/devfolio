ca5481b8a93a0043ccf0343ad6e8362a6feaefd0
Brooke Krutsch <krutschb@gmail.com>
2022-07-14 22:23:53 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -94,6 +94,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
